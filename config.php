37d34984d98802abea419f3f6cf449ee56c17adc
xervers <info@xervers.net>
2016-01-27 12:48:46 +0100

    Create config.php
    Default configuration file with the necessary data to access the servers.

---
+++ config.php
@@ -0,0 +1,20 @@
+<?php
+// MySQL Server Access Information
+$mysql_host = '127.0.0.1';
+$mysql_port = 3385;
+$mysql_user = 'mysql_user';
+$mysql_pass = 'mysql_pass';
+
+// Teamspeak 3 Server Access Information
+$ts3_host = '127.0.0.1';
+$ts3_query = 10011;
+$ts3_voice = 9987;
+$ts3_user = 'serveradmin';
+$ts3_pass = 'pa$$w0rd';
+
+// JTS3ServerMod Access Information
+$jts3_host = '127.0.0.1';
+$jts3_port = 5873;
+$jts3_pass = 'Pa$$w0rd';
+
+?>
